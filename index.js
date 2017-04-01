c292282060e18be127c7aa0e6d4eea0372a2f7be
yu kawase <kawase@aloe-project.com>
2017-04-01 23:41:56 +0900

    m

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const WAMP_SERVER = require('wamp-server');
 const SERVER = new WAMP_SERVER({
-  port: 8000,
+  port: process.env.PORT || 8000,
   realms: ['realm1'], // array or string 
 });
 // to close the server - SERVER.close();
