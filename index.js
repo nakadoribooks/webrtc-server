2631ed122d57fbb0cb0c3022e15a3c3877ff41c1
yu kawase <kawase@aloe-project.com>
2017-04-02 02:35:14 +0900

    m

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-'use strict';
-const WAMP_SERVER = require('wamp-server');
-const SERVER = new WAMP_SERVER({
-  port: process.env.PORT || 8000,
-  realms: ['realm1'], // array or string 
-});
-// to close the server - SERVER.close();
+// 'use strict';
+// const WAMP_SERVER = require('wamp-server');
+// const SERVER = new WAMP_SERVER({
+//   port: process.env.PORT || 8000,
+//   realms: ['realm1'], // array or string 
+// });
 
 
+WAMPRT_TRACE = true;
+
+var Router = require('wamp.rt');
+var program = require('commander');
+
+program
+    .option('-p, --port <port>', 'Server IP port', parseInt,8000);
+
+//
+// WebSocket server
+//
+var app = new Router(
+    { port: program.port}
+);
